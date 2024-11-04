77962673b3b57326a0e2d30435697f7a4e76b2fa
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-04 16:29:34 +0100

    Made button to choose grid size
    Made the grid react to hover and change color

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const W_UNIT = 4;
 const H_UNIT = 4;
 const container = document.querySelector("#container");
 
+const button = document.querySelector("#btn");
+button.textContent = "Number squares?";
+
+
 //function to draw the grid
 function drawGrid(width, height){
     for(let i = 1; i <= width*height; i++){
         const cell = document.createElement("div");
-    
         cell.classList.add("cell");
         cell.textContent = i;
         container.appendChild(cell);
-    
-        // cell.addEventListener('mouseenter',(e) => {
-        //     console.log(`mouse enter ${i}`);
-        // });
-    } 
-}
 
-function hoverEffect(){
-    
+        cell.addEventListener('mouseenter',() => {
+            console.log("mouse enter");
+            cell.classList.add("new")
+        });
+    } 
 }
 
 drawGrid(W_UNIT, H_UNIT);
