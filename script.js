419126be97085a6a732d8899d2f27aac2e595429
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-04 16:49:34 +0100

    Add button listener to input grid size

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const W_UNIT = 4;
-const H_UNIT = 4;
 const container = document.querySelector("#container");
+const btn = document.querySelector("#button");
+btn.textContent = "grid width size?";
 
-const button = document.querySelector("#btn");
-button.textContent = "Number squares?";
-
+btn.addEventListener("click",() => {
+    let btnNumSqrs = prompt("enter the size of the grid");
+    tempValue = btnNumSqrs;
+});
 
 //function to draw the grid
-function drawGrid(width, height){
-    for(let i = 1; i <= width*height; i++){
+function drawGrid(width){
+    for(let i = 1; i <= width*width; i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         cell.textContent = i;
         container.appendChild(cell);
 
-        cell.addEventListener('mouseenter',() => {
-            console.log("mouse enter");
+        cell.addEventListener("mouseenter",() => {
             cell.classList.add("new")
         });
     } 
 }
 
-drawGrid(W_UNIT, H_UNIT);
+drawGrid(4);
