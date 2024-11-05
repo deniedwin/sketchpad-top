89e6152d91f9f81d9a35113ccb02203cb6346654
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-05 11:54:02 +0100

    Received the width size of css container
    Used the js .innedWidth to get this size
    
    Using this size I calculated the cell width

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-let btnNumSqrs = 0;
 const container = document.querySelector("#container");
+const DISPLAY_SIZE = container.offsetWidth; //get container size from css file
+
 const btn = document.querySelector("#button");
 btn.textContent = "change # squares";
 
 btn.addEventListener("click", () => {
-    btnNumSqrs = parseInt(prompt("Choose a number of squares per side"), 10);
-    if (btnNumSqrs > 0) {
+    let btnNumSqrs = parseInt(prompt("Choose a number of squares per side"), 10);
+    if (btnNumSqrs > 0 && btnNumSqrs <= 100) {
         container.textContent = ""; // Clear previous grid
         drawGrid(btnNumSqrs);
     }
-});
+    else{
+        alert("Choose size between 1 and 100");
+    }
+}); 
 
 // Function to draw the grid
 function drawGrid(width) {
-    const cellSize = 400 / width; // Calculate cell size in pixels for 400px container
+    const cellSize = DISPLAY_SIZE / width; // Calculate cell size in pixels for 400px container
 
     for (let i = 1; i <= width * width; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
-
-        cell.style.width = `${cellSize}px`;   // Set cell width in pixels
-        cell.style.height = `${cellSize}px`;  // Set cell height in pixels
-        cell.textContent = i;
+        cell.style.flex = `${cellSize}px 0 0`;
+        // cell.textContent = i;
         container.appendChild(cell);
 
         cell.addEventListener("mouseenter", () => {
